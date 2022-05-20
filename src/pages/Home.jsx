9cf51f3df1be5055c41d40f7d6b07e98fcc4f63b
Ikhlasdev17 <iklasaralbaev@gmail.com>
2022-05-20 11:57:42 +0500

    project started

---
+++ src/pages/Home.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+      <div className='app'>
+        <div className="home-container">
+            <div className="cols">
+                <h1>Ma'ruzalar</h1>
+            </div>
+            <div className="cols">
+                <h1>Amaliyotlar</h1>
+            </div>
+            <div className="cols">
+                <h1>Labaratoriya</h1>
+            </div>
+            <div className="cols">
+                <h1>Mustaqil ta'lim</h1>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Home
